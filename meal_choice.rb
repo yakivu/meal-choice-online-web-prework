22c6ad013af7c23aec393850bdf700aafd8057e4
Adam Walker <adamw207@yahoo.com>
2019-02-28 18:49:51 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,2 @@
-# Define a method meal_choice that returns the meal_choice that was
+def meal_choice(name="meat", name="vegan") # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
